550861aa7ba7659639d7cb29aeb332080a792e8a
Jose_Varela <jose@josevarela.net>
2023-05-26 20:44:56 -0600

    Optimiza llamada de historial de mapa

--- Player.gd
+++ Player.gd
@@ -1,16 +1,17 @@
 extends CharacterBody2D	
 
+"""
+NOTA
+
+Intentar realizar todo el proceso por medio de tiles, y calcular el normal
+basado en la posición del tile disponible de la posición actual del jugador.
+"""
+
+var destination = null
 const SPEED = 300.0
-#var direction = Vector2(0,0)
-# const JUMP_VELOCITY = -400.0
-# const TileSize = 128
-# var curPos = Vector2i(0,0)
-# var VisitedPositions = []
-# var children = []
-var LastPosition = Vector2(0,0)
 const DEBUG = true
 
-signal collided(given_collision)
+signal TouchedTile(given_collision)
 
 func getSensorStatus():
 	return [
@@ -25,55 +26,91 @@ func allSensorsDisabled():
 		not $SensorAbajo.is_colliding() and
 		not $SensorArriba.is_colliding() and
 		not $SensorDerecha.is_colliding())
+		
+func getTilePosition():
+	var TileMapExp = get_parent().get_node("Exploracion") as TileMap
+	var pos = TileMapExp.local_to_map( global_position ) as Vector2i
+	return pos
 
 """
 Retorna verdadero o falso si el bloque pedido de position - offset ya fue explorado.
 """
-func wasTileAlreadyExplored(position : Vector2, offset : Vector2i):
+func wasTileAlreadyExplored(curPosition : Vector2i, offset : Vector2i):
 	var TileMapExp = get_parent().get_node("Exploracion") as TileMap
-	var pos = TileMapExp.local_to_map( global_position ) as Vector2i
+	var ColisionMap = get_parent().get_node("TileMap") as TileMap
+	var pos = curPosition
 	pos += offset
 	var data = TileMapExp.get_cell_atlas_coords(0, pos) as Vector2i
+	var colisionData = ColisionMap.get_cell_atlas_coords(0, pos) as Vector2i
 	
 	if data:
-		print(data)
-		return data == Vector2i(1,0)
+		# print(curPosition, offset, data, colisionData)
+		return data == Vector2i(1,0) and colisionData == Vector2i(0,0)
 	
 	return false
+	
+func currentTileHasBeenVisited(curPosition : Vector2i):
+	return wasTileAlreadyExplored(curPosition, Vector2i(0,0))
 
-func _physics_process(delta):
+func tilePositionToGlobal(tilePosition : Vector2i):
+	var TileMapExp = get_parent().get_node("Exploracion") as TileMap
+	return TileMapExp.map_to_local(tilePosition)
+	
+func MoveToNewLocation(delta : float, newposition : Vector2i):
+	destination = newposition
+	var GlobalNewPosition = tilePositionToGlobal(newposition)
+	# print( global_position, GlobalNewPosition )
+	global_position = global_position.move_toward(GlobalNewPosition, delta*SPEED )
+	
+func DetermineNewDestination():
+	var current_position = getTilePosition()
+	
+	# Si estamos actualmente en un bloque ya visitado, encuentra una manera de salir de ahi.
+	if currentTileHasBeenVisited(current_position):
+		print("Current tile has been visited")
+		if not wasTileAlreadyExplored(current_position, Vector2i(0,1)) and not $SensorAbajo.is_colliding():
+			# print("abajo")
+			current_position.y += 1
+			return current_position
+		if not wasTileAlreadyExplored(current_position, Vector2i(1,0)) and not $SensorDerecha.is_colliding():
+			# print("derecha")
+			current_position.x += 1
+			return current_position
+		if not wasTileAlreadyExplored(current_position, Vector2i(0,-1)) and not $SensorArriba.is_colliding():
+			# print("derecha")
+			current_position.y -= 1
+			return current_position
+		return null
+	
+	# Abajo
+	if (wasTileAlreadyExplored(current_position, Vector2i(0,-1))):
+		if $SensorAbajo.is_colliding():
+			return null
+		# print("abajo")
+		current_position.y += 1
+		return current_position
+	# Derecha
+	if (wasTileAlreadyExplored(current_position, Vector2i(-1,0))):
+		print("Derecha")
+		if $SensorDerecha.is_colliding():
+			return null
+		current_position.x += 1
+		return current_position
+	# Izquierda
+	if (wasTileAlreadyExplored(current_position, Vector2i(1,0))):
+		# print("Izquierda")
+		if $SensorIzquierda.is_colliding():
+			return null
+		current_position.x -= 1
+		return current_position
+	return null
+	
+func _process(delta):
 	#var pos = get_parent().get_node("Exploracion").local_to_map( global_position ) as Vector2i
-	#print( wasTileAlreadyExplored(global_position, Vector2i(0,0)) )
+	## print( wasTileAlreadyExplored(global_position, Vector2i(0,0)) )
 	# Situación donde nos quedamos en limbo, dado la distancia de los sensores, no detectan ninguna pared.
-	if allSensorsDisabled():
-		velocity = Vector2(0,0)
+	var newposition = getTilePosition()
 		
-		if not wasTileAlreadyExplored(global_position, Vector2i(0,1)):
-			velocity.y = SPEED * 1
-		if not wasTileAlreadyExplored(global_position, Vector2i(0,-1)):
-			velocity.y = SPEED * -1
-		
-		move_and_slide()
-		pass
-	
-	for i in get_slide_collision_count():
-		var collision = get_slide_collision(i)
-		if collision:
-			collided.emit(collision)
-			
-	# Situacion donde el personaje se encuentra en una area horizontal.
-	if $SensorArriba.is_colliding() != $SensorAbajo.is_colliding():
-		if not $SensorAbajo.is_colliding():
-			velocity.y = SPEED * 1
-		if not $SensorArriba.is_colliding():
-			velocity.y = SPEED * -1
-	# Situación donde el personaje se encuentra en una area vertical.
-	if $SensorDerecha.is_colliding() != $SensorIzquierda.is_colliding():
-		if not $SensorDerecha.is_colliding():
-			velocity.x = SPEED * 1
-		if not $SensorIzquierda.is_colliding():
-			velocity.x = SPEED * -1
-	
 	if DEBUG:
 		var stats = getSensorStatus()
 		# print(stats)
@@ -81,84 +118,21 @@ func _physics_process(delta):
 		$"L-ABA".set_text( "true" if stats[1] else "false" )
 		$"L-ARR".set_text( "true" if stats[2] else "false" )
 		$"L-DER".set_text( "true" if stats[3] else "false" )
-		
-	move_and_slide()
 	
-	LastPosition = self.position
-
-"""
-# Determine if the current position given has never been seen before.
-func calculate_pos_and_determine( currentPosition : Vector2i, pastPositions ):
-	if currentPosition in pastPositions:
-		return false
-	return
-	
-func generateChildren():
-	$TileMap.
-	var sStatus = getSensorStatus()
-	
-	print(sStatus)
-	if not sStatus[0]:
-		var aux = self
-		aux.curPos[0] -= 1
-		children.append(aux)
-		
-	if not sStatus[1]:
-		var aux = self
-		aux.curPos[1] += 1
-		children.append(aux)
-	
-	if not sStatus[2]:
-		var aux = self
-		aux.curPos[1] -= 1
-		children.append(aux)
-		
-	if not sStatus[3]:
-		var aux = self
-		aux.curPos[0] += 1
-		children.append(aux)
-		
-	print(children)
-	
-var testResult = [1,4]
-func searchBPP(goal):
-	if ConvertVectorToPosition(self.position) == goal:
-		print("goal")
-		return [self]
-	if self in VisitedPositions:
-		print("already visited")
-		return null
-	generateChildren()
-	VisitedPositions.append( self )
-	for c in children:
-		var res = c.searchBPP(goal)
-		if res != null:
-			return res
-	print("faled")
-	return null
-	
-func ConvertPositionToVector(pos : Vector2i):
-	print(pos)
-	var newpos = pos
-	newpos.x *= TileSize
-	newpos.y *= TileSize
-	return newpos
-	
-func ConvertVectorToPosition(pos : Vector2):
-	print(pos)
-	var newpos = pos
-	newpos.x /= TileSize
-	newpos.y /= TileSize
-	return newpos
-
-func _ready():
-	print( searchBPP( Vector2(1,4) ) )
-	self.position = ConvertPositionToVector(curPos)
-"""
-
-# func _physics_process(delta):
-	#velocity.x = SPEED * direction.x
-	#velocity.y = SPEED * direction.y
-	#print(self.position)
-	
-	#move_and_slide()
+	if destination != null:
+		var GlobalNewPosition = tilePositionToGlobal(destination)
+		print(newposition, destination)
+		if (destination.y > newposition.y) and $SensorAbajo.is_colliding():
+			destination = null
+			TouchedTile.emit( newposition )
+			return
+		# print( newposition, destination )
+		global_position = global_position.move_toward(GlobalNewPosition, delta*SPEED )
+		if GlobalNewPosition == global_position:
+			TouchedTile.emit( newposition )
+			destination = null
+		return
+	else:
+		var newLocation = DetermineNewDestination()
+		if newLocation:
+			MoveToNewLocation(delta, newLocation)
--- laberinto.gd
+++ laberinto.gd
@@ -1,50 +1,35 @@
 extends Node2D
 
-var lastCell
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	var cell = $Exploracion.local_to_map( $Personaje.global_position ) as Vector2i
-	cell.x += 1
-	cell.y += 1
-	var data = $Exploracion.get_cell_tile_data(0, cell) as TileData
-	if data:
-		lastCell = cell
-	pass # Replace with function body.
-	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	var cell = $Exploracion.local_to_map( $Personaje.global_position ) as Vector2i
+var lastCell = Vector2i()
+
+func getTilePosition():
+	var pos = $Exploracion.local_to_map( $Personaje.global_position ) as Vector2i
+	return pos
+
+func MarkCellIfPassed():
+	var cell = getTilePosition()
 	var data = $Exploracion.get_cell_tile_data(0, cell) as TileData
 	if cell != lastCell:
 		print(cell)
 		if lastCell != null:
-			$Exploracion.set_cell(
-				0,
-				lastCell,
-				0,
-				Vector2i(1,0)
-				)
-		lastCell = cell
-	"""
-	if data:
-		var explorado = data.get_custom_data("Explorado")
-		if not explorado:
-			data.set_custom_data("Explorado", true)
-			print("cambiado")
-			print(explorado)
-		else:
-			print(cell, "ya explorado")
-	"""
-	pass
-
-
-func _on_personaje_collided(collision):
-	pass
-	"""
-	if collision.collider is TileMap:
-		var cell = $Exploracion.local_to_map( $Personaje.global_position ) as Vector2i
-		var data = $Exploracion.get_cell_tile_data(0, cell) as TileData
-		if data:
-			# Marca el area como explorado.
 			$Exploracion.set_cell(0,lastCell,0,Vector2i(1,0))
-	"""
+		lastCell = cell
+		
+func ForceSetTileAsPassed(cellPosition : Vector2i):
+	$Exploracion.set_cell(0,cellPosition,0,Vector2i(1,0))
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	# Marca el cuadro inicial como ya encontrado, para hacer que el personaje se mueva.
+	lastCell = getTilePosition()
+	$Exploracion.set_cell(0,lastCell,0,Vector2i(1,0))
+	MarkCellIfPassed()
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	# MarkCellIfPassed()
+
+func _on_personaje_touched_tile(given_collision : Vector2i):
+	ForceSetTileAsPassed(given_collision)
+	pass # Replace with function body.
